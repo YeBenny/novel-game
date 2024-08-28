b9f1ef1b088cbf35e31565d3164ea6cb06eaccd4
jennyfeng <645809333@qq.com>
2024-08-28 16:40:03 +0800

    <feat>(redux): Add redux framework

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,18 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
-import { I18nextProvider } from 'react-i18next';
-
-import i18n from './i18n/i18n.ts';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import store from './redux/store'
+import { Provider } from 'react-redux'
+import { I18nextProvider } from 'react-i18next'
+import i18n from './i18n/i18n'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <I18nextProvider i18n={ i18n }>
-      <App />
-    </I18nextProvider>
+    <Provider store={ store }>
+      <I18nextProvider i18n={ i18n }>
+        <App />
+      </I18nextProvider>
+    </Provider>
   </StrictMode>,
-);
+)
