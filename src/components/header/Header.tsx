5c561f77f57cc055d7722344348a3674f55a377f
jennyfeng <645809333@qq.com>
2024-08-29 16:47:47 +0800

    <feat>(router): Apply the react-router framework

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,20 @@
+import { Link } from 'react-router-dom'
+import './Header.module.css'
+
+export default function Header() {
+  return (
+    <>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/counter">Counter</Link>
+        </li>
+        <li>
+          <Link to="/welcome">Welcome</Link>
+        </li>
+      </ul>
+    </>
+  )
+}
