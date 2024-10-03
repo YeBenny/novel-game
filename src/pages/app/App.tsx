bc932fa58bdf0b1eba73977a0c870d6e26116d60
YE WEIBIN <yeshinebenny@gmail.com>
2024-10-03 15:21:13 +0800

    <css>(*): update css files

--- src/pages/app/App.tsx
+++ src/pages/app/App.tsx
@@ -1,14 +1,5 @@
-import './App.css'
-
-// Define a language switcher, matching with the resources in `src/i18n/i18n.ts`,
-// or the file `public/locales`, keeping the file structure untouched.
-// The manually selected language is persisted in the localStorage, in the field of `i18nextLng`
 function App() {
-  return (
-    <>
-      <div className="App-header"></div>
-    </>
-  )
+  return <></>
 }
 
 export default App
